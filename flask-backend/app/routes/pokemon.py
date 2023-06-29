382b5a60f0e85c284b86357bcb69ecafc2d1c367
ChrisEkeDev <iam@chriseke.com>
2023-06-29 14:57:15 -0500

    First 3 get routes

--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -1,3 +1,22 @@
 from flask import Blueprint
+from app.models import Pokemon, PokemonTypes
+from random import randint
 
 pokemon = Blueprint("pokemon", __name__)
+
+
+@pokemon.route('/')
+def get_all_pokemon():
+    pokemon = Pokemon.query.all()
+    return pokemon
+
+@pokemon.route('/types')
+def get_pokemon_types():
+    types = PokemonTypes.query.all()
+    return types
+
+@pokemon.route('/random')
+def get_random_pokemon():
+    pokemon = Pokemon.query.all()
+    randomPokemon = pokemon[randint(0, len(pokemon))]
+    return randomPokemon
