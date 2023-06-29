f16cfb3895abfa3da6a83d00e51918a0b30a9a42
Roman Barbalat <roman.barbalat22@gmail.com>
2023-06-29 17:39:50 -0400

    started items

--- flask-backend/app/forms/item_form.py
+++ flask-backend/app/forms/item_form.py
@@ -1,10 +1,11 @@
-from wtforms import Strinfield, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, URL, Length
 
-class ItemForm(FlaskForm):
-  happiness = Integerfield("Happiness", validators=[DataRequired()])
-  imageUrl = Stringfield("imageUrl", validators=[DataRequired(), URL()])
-  name = Stringfield("Name", validators=[DataRequired(), Length(max=255)])
+class EditItemForm(FlaskForm):
+  happiness = IntegerField("Happiness", validators=[DataRequired()])
+  imageUrl = StringField("imageUrl", validators=[DataRequired(), URL()])
+  name = StringField("Name", validators=[DataRequired(), Length(max=255)])
   price = IntegerField("Price", validators=[DataRequired()])
-  pokemonId = 
+  pokemonId = IntegerField("Pokemon", validators=[DataRequired()])
+  submit = SubmitField("submit")
--- flask-backend/app/routes/items.py
+++ flask-backend/app/routes/items.py
@@ -1,13 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from ..models import db, Item
+from app.forms.item_form import EditItemForm
 
 
 item = Blueprint("item", __name__)
 
-@item.route('/<int:id>')
+@item.route('/<int:id>', methods = ["PUT"])
 def edit_item(id):
+  form = EditItemForm()
+
+  form['csrf_token'].data = request.cookies['csrf_token']
+  # form["name"].data = request.form["name"]
+  item = Item.query.get(id)
+  print("form data   ", form.data)
+  print("item    ", item)
+  print("blahblah  ", request.data)
+  return {"msg": "received"}
 
 
+#works w/o specifying method =["DELETE"]
 @item.route('/<int:id>')
 def delete_item(id):
   item = Item.query.get(id)
