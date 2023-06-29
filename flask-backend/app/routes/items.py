2b31e323864259fd549690462fd2fd7f828bcac9
Roman Barbalat <roman.barbalat22@gmail.com>
2023-06-29 18:33:50 -0400

    fixed update route

--- flask-backend/app/routes/items.py
+++ flask-backend/app/routes/items.py
@@ -1,21 +1,32 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from ..models import db, Item
 from app.forms.item_form import EditItemForm
 
 
 item = Blueprint("item", __name__)
 
-@item.route('/<int:id>', methods = ["PUT"])
+@item.route('/<int:id>', methods = ["POST"])
 def edit_item(id):
-  form = EditItemForm()
-
-  form['csrf_token'].data = request.cookies['csrf_token']
-  # form["name"].data = request.form["name"]
   item = Item.query.get(id)
-  print("form data   ", form.data)
-  print("item    ", item)
-  print("blahblah  ", request.data)
-  return {"msg": "received"}
+  # print("item   ", item)
+  form = EditItemForm()
+  # data = request.get_json()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  # print(form.data)
+  # print(item.name)
+  # print(form.data["name"])
+  item.name = form.data["name"]
+  item.imageUrl = form.data["imageUrl"]
+  item.happiness = form.data["happiness"]
+  item.price = form.data["price"]
+  db.session.commit()
+
+
+  # print("form data")
+  # print("item    ", item)
+  # print("blahblah  ", request.data)
+  # return {"msg": "blah"}
+  return request.get_json()
 
 
 #works w/o specifying method =["DELETE"]
